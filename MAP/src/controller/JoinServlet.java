c93b04c634342590709b2679475e72f132878c08
QMCHE <gaga1421@naver.com>
2018-10-27 12:26:35 +0900

    join

--- MAP/src/controller/JoinServlet.java
+++ MAP/src/controller/JoinServlet.java
@@ -1,11 +1,16 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import db.api.AccountMgr;
+import db.bean.MemberBean;
 
 /**
  * Servlet implementation class JoinServlet
@@ -13,14 +18,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/join")
 public class JoinServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public JoinServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public JoinServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -34,7 +39,30 @@ public class JoinServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		request.setCharacterEncoding("UTF-8");
+		String userid = request.getParameter("userid");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
 
+		MemberBean member = new MemberBean();
+		member.setId(userid);
+		member.setPassword(password);
+		member.setName(name);
+		member.setEmail(email);
+
+		AccountMgr account = new AccountMgr();
+		int result = account.procRegister(member);
+
+		if(result==1) {
+			HttpSession session = request.getSession();
+			session.setAttribute("user", member);
+			request.getRequestDispatcher("home.jsp").forward(request, response);
+		}
+		else {
+			request.getRequestDispatcher("join.jsp").forward(request, response);
+		}
+		return;
+	}
+	
 }
