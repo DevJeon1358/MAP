6bee9123d624dad98d0687308b9af06f3100dd38
LJY1022 <43703494+LJY1022@users.noreply.github.com>
2018-10-27 14:48:45 +0900

    home

--- MAP/src/controller/NewServlet.java
+++ MAP/src/controller/NewServlet.java
@@ -1,11 +1,18 @@
 package controller;
 
 import java.io.IOException;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import db.api.ProjectMgr;
+import db.bean.ProjectBean;
 
 /**
  * Servlet implementation class New
@@ -33,8 +40,31 @@ public class NewServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		
+		String name = request.getParameter("projectname");
+		String subject = request.getParameter("subject");
+		String due = request.getParameter("endtime");
+		Date date;
+		try {
+			date = new SimpleDateFormat("yyyy/MM/dd HH:mm").parse(due);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			date = new Date();
+		}
+		
+		ProjectBean project = new ProjectBean();
+		project.setName(name);
+		project.setSubject(subject);
+		project.setDue(date);
+		
+		ProjectMgr pm = new ProjectMgr();
+		pm.addProject(project);
+		
+		// 프로젝트 멤버 추가하기
+		
+		// home으로 redirect
+		request.getRequestDispatcher("home.jsp").forward(request, response);
 	}
 
 }
